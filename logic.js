39a93bf24a7ceae3b4e64b17c9fbe65f49079d8d
Andrew Siegel <siegelal7@gmail.com>
2020-09-03 19:15:37 -0400

    added preventDefault to submit

--- logic.js
+++ logic.js
@@ -1,5 +1,6 @@
 var buttonLink = document.getElementById("linkToLinkedin");
 
-buttonLink.addEventListener("click", function () {
+buttonLink.addEventListener("click", function (event) {
+  event.preventDefault();
   window.open("https://www.linkedin.com/in/andrew-siegel-54286447/", "_blank");
 });
