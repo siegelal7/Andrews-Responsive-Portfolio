e2ee410f145a7dc8ddffdb3169811aa35ca41939
Andrew Siegel <siegelal7@gmail.com>
2020-09-03 18:40:21 -0400

    added a new button on contact to link to linkedin

---
+++ logic.js
@@ -0,0 +1,5 @@
+var buttonLink = document.getElementById("linkToLinkedin");
+
+buttonLink.addEventListener("click", function () {
+  window.open("https://www.linkedin.com/in/andrew-siegel-54286447/", "_blank");
+});
