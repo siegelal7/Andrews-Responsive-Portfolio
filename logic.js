2bc6310fd14e391e153a366166e7ec410417f841
Andrew Siegel <siegelal7@gmail.com>
2020-10-20 22:51:38 -0400

    added proj 2 to portfolio

--- logic.js
+++ logic.js
@@ -1,8 +1,8 @@
 // var buttonLink = document.getElementById("linkToLinkedin");
 var planner = $("#planner");
-
 var weather = $("#weather");
 var proj = $("#proj1");
+var projTwo = $("#proj2");
 
 planner.on("click", function (event) {
   event.preventDefault();
@@ -15,4 +15,8 @@ weather.on("click", function (event) {
 proj.on("click", function (event) {
   event.preventDefault();
   window.open("https://siegelal7.github.io/Find-Your-Adventure/", "_blank");
+});
+projTwo.on("click", function (event) {
+  event.preventDefault();
+  window.open("https://seekr-jobs.herokuapp.com/", "_blank");
 });
